a2e8cc7a130bd26cc4ff1ff2c564937bd82e3147
Kusay-1234 <183075525+Kusay-1234@users.noreply.github.com>
2024-10-05 12:42:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const item=document.querySelector('#item');
+const  todobox=document.querySelector('#box1');
+
+item.addEventListener(
+    "keyup",
+         function(event)
+         {
+            if(event.key=="Enter"){
+                addToDo(this.value)
+                this.value=""
+            }
+
+      }
+    
+)
+const addToDo = (item)=>{
+    const listItem=document.createElement("li");
+    listItem.innerHTML=`${item}`;
+
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle('done');
+        }
+    )
+
+    todobox.appendChild(listItem)
+}
